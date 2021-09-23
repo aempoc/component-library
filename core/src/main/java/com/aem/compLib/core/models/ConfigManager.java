f1fc4cd360f51d7506910754ae57590f14c661cc
AEM POC <aempoc.2021@gmail.com>
2021-09-23 07:58:38 -0400

    article list

--- core/src/main/java/com/aem/compLib/core/models/ConfigManager.java
+++ core/src/main/java/com/aem/compLib/core/models/ConfigManager.java
@@ -1,19 +1,53 @@
 package com.aem.compLib.core.models;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
 
 @Model(
 		adaptables = Resource.class,
-		adapters = ConfigManager.class,
 		defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
 )
-public interface ConfigManager {
+public class ConfigManager {
+	
 	@Inject
-	public String getTheme();
+	private String theme;
+	
 	@Inject
-	public String getSitename();
+	private String sitename;
+	
+	@SlingObject
+    private Resource currentResource;
+	
+    @SlingObject
+    private ResourceResolver resourceResolver;
+    
+    private String currentPagePath;
+    
+    @PostConstruct
+    protected void init() {
+    	PageManager pageManager= currentResource.getResourceResolver().adaptTo(PageManager.class);
+    	Page currentPage = pageManager.getContainingPage(currentResource);
+    	currentPagePath = currentPage.getPath();
+    }
+    
+    public String getCurrentPagePath() {
+    	return currentPagePath;
+    }
+	
+	public String getTheme() {
+		return theme;
+	}
+	
+	public String getSitename() {
+		return sitename;
+	}
 }
